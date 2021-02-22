e01b6800babd27c857fcaa1083a238ebb81afd07
luoyinfeng <1515357383@qq.com>
2021-02-22 17:30:20 +0800

    Initial commit

---
+++ src/main/java/com/example/wzgl/WzglApplication.java
@@ -0,0 +1,13 @@
+package com.example.wzgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WzglApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wzgl/WzglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wzgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
